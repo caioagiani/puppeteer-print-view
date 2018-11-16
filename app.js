05231313314da714c3c9df8cb48b7ddc7bebf495
Saviobosco <Johnboscoomebe@yahoo.com>
2018-11-16 14:32:05 +0100

    Updated files

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const puppeteer = require('puppeteer');
 // create a new page 
 // open the specified page 
 // wait for the specified time
-//
+// open a new page after 7 mins
+
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
          args: [
              '--start-maximized',
              '--proxy-server=x.botproxy.net:8080',
@@ -19,23 +20,24 @@ const puppeteer = require('puppeteer');
              '--window-size=1920x1080',
             ],
         });
-    const page = await browser.newPage();
-    page.authenticate({
-        username: 'pxu10789-0',
-        password: '0qrnkBQa=phu6kW294dU'
-    });
-    let i = await page.goto('https://www.youtube.com/watch?v=z5fsL-5Fc-I',{timeout : 0,waitUntil: 'domcontentloaded'});
-    //await page.screenshot({path: 'trademutualfund.png'});
-    await page.waitFor(60000 * (Math.ceil(Math.random() * 4) + 2));
+        for (let num = 0; num < 10; num++) {
+            // using promise 
+            const testAsync = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve();
+                },60000 * 2);
+            });
+            await testAsync();
+            const page = await browser.newPage();
+            page.authenticate({
+            username: 'pxu10789-0',
+            password: '0qrnkBQa=phu6kW294dU'
+            });
+            await page.goto('https://www.youtube.com/watch?v=z5fsL-5Fc-I',{timeout : 0,waitUntil: 'domcontentloaded'});
+            //await page.screenshot({path: 'trademutualfund.png'});
+            await page.waitFor(60000 * (Math.ceil(Math.random() * 4) + 2));
+            console.log(`page ${num}`);
+        };
     await browser.close();
     await browser.close();
   })();
-/*for (let num = 0; num < 50; num++) {
-    setTimeout(
-        () => {
-            
-        },
-          420000
-    );
-    console.log(num);
-};*/
